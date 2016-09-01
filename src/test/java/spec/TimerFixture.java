2459aece86b7a0ef67364ad9ad29e9f173f8c71f
Prashant B <prashant_bhikhu@hotmail.com>
2016-09-01 13:02:43 +1200

    Added Code to debug the way example works and how its Before Example time is calculated.

--- src/test/java/spec/TimerFixture.java
+++ src/test/java/spec/TimerFixture.java
@@ -29,6 +29,14 @@ public class TimerFixture {
         return p + new Random().nextInt();
     }
 
+    public void sleep() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public boolean checkFirstNameAsEmptyString() {
         return checkFirstName("");
     }
