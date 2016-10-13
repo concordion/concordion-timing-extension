afa6434ce330acef2d0364fb8a92f87dc021fb42
Nigel Charman <nigel.charman.nz@gmail.com>
2016-10-13 17:46:59 +1300

    Publish of Github pages from Gradle.

--- spec/timingExtensionToggle.js
+++ spec/timingExtensionToggle.js
@@ -1,24 +1,28 @@
 window.onload = function() {
 
     var toggleButton = document.getElementById("toggleImg");
+    toggleButton.onclick = handleToggle;
 
-    toggleButton.onclick = function() {
-
-        var display = false;
-        // check to see if the button has the class with the on styling
-        if (toggleButton.classList.contains("time-toggle-button-on")) {
-            // show it as off
-            toggleButton.classList.remove("time-toggle-button-on");
-        } else {
-            // show it as on
-            toggleButton.classList.add("time-toggle-button-on");
-            display = true;
-        }
-
-        // update the time figures to reflect the state of the button
-        toggleTiming(display);
-    };
+    toggleTiming(toggleButton.classList.contains("time-toggle-button-on"));
 };
+
+function handleToggle() {
+    var display = false;
+    var toggleButton = document.getElementById("toggleImg");
+
+    // check to see if the button has the class with the on styling
+    if (toggleButton.classList.contains("time-toggle-button-on")) {
+        // show it as off
+        toggleButton.classList.remove("time-toggle-button-on");
+    } else {
+        // show it as on
+        toggleButton.classList.add("time-toggle-button-on");
+        display = true;
+    }
+
+    // update the time figures to reflect the state of the button
+    toggleTiming(display);
+}
 
 function toggleTiming(show) {
 
